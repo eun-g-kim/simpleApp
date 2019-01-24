f68bbff3765e4108dde1996bf647bddb45651e08
eun-g-kim <43198923+eun-g-kim@users.noreply.github.com>
2019-01-25 00:36:31 +0900

    Update MainActivity.java

--- src/main/java/com/example/example/MainActivity.java
+++ src/main/java/com/example/example/MainActivity.java
@@ -2,49 +2,62 @@ package com.example.example;
 
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity {
 
     private TextView ctr;
-    private GoogleMap mMap;
-
+    private ImageView img;
+    private int value =0 ; //카운터 값
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("ex");  //타이틀 공백처리
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //왼쪽에 home버튼 (뒤로가기 버튼 추가)
-        getSupportActionBar().setHomeAsUpIndicator(Drawable.createFromPath("@android:drawable/ic_menu_sort_by_size"));  //home 버튼 모양 변경 가능
-
+      //  getSupportActionBar().setDisplayHomeAsUpEnabled(true); //왼쪽에 home버튼 (뒤로가기 버튼 추가)
+       // getSupportActionBar().setHomeAsUpIndicator(Drawable.createFromPath("@android:drawable/ic_menu_sort_by_size"));  //home 버튼 모양 변경 가능
+       getSupportActionBar().setTitle("");   //타이틀 공백처리
         ctr = findViewById(R.id.counter);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.g_map);
-        mapFragment.getMapAsync(this);
+        img = findViewById(R.id.mapImage);
+        ctr.setText(Integer.toString(value)); //항상 0으로 초기화
         ctr.setOnClickListener(ctrListener); //리스너 부착
+        img.setOnClickListener(imgListener);
+
+
     }
+
+    View.OnClickListener ctrListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "클릭됨", Toast.LENGTH_LONG).show();
+           // img.setVisibility(View.VISIBLE);
+            ++value;
+            ctr.setText(Integer.toString(value));
+        }
+    };
+    View.OnClickListener imgListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "클릭됨", Toast.LENGTH_LONG).show();
+            img.setVisibility(View.GONE);
+        }
+    };
 
     //추가된 소스, ToolBar에 menu.xml을 인플레이트함
     @Override
@@ -74,25 +87,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-
     public void onClick(View view) {
     }
 
-    //map 관련- 출처:http://www.zoftino.com/android-google-maps-tutorial
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        //seattle coordinates
-        LatLng seattle = new LatLng(47.6062095, -122.3320708);
-        mMap.addMarker(new MarkerOptions().position(seattle).title("Seattle"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(seattle));
-    }
-
-    View.OnClickListener ctrListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(getApplicationContext(), "클릭됨", Toast.LENGTH_LONG).show();
-        }
-    };
 }
 
